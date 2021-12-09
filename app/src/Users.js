cf16bef8bd2e1c91d593c807fb344f0cee048a11
carmegarut <carmencgu3@gmail.com>
2021-12-09 16:46:31 +0100

    made header responsive

--- app/src/Users.js
+++ app/src/Users.js
@@ -1,10 +1,12 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Container, Card, Stack, Badge } from 'react-bootstrap'
+import { Container, Card, Stack, Badge, Form } from 'react-bootstrap'
 import { usersInit } from './reducers/usersReducers'
 
 export default function Users () {
   const users = useSelector(state => state.users)
+  const [search, setSearch] = useState('')
+
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -13,29 +15,41 @@ export default function Users () {
 
   return (
     <Container>
-      {console.log(users)}
-      {users.map(user => (
-        <Card key={user.id}>
-          <Card.Body>
-            <Stack direction='horizontal' gap={2}>
-              <img
-                style={{
-                  width: '150px',
-                  height: '150px',
-                  resizeMode: 'contain'
-                }}
-                src={user.profileImg}
-                alt={user.name}
-              />
-              <div>
-                <Card.Title>{user.name} {user.surname}
-                  {' '}<Badge bg='info'>{user.trustRate} % trust</Badge>
-                </Card.Title>
-                <Card.Text>Email: {user.email}</Card.Text>
-              </div>
-            </Stack>
-          </Card.Body>
-        </Card>))}
+      <h1>Users</h1>
+      <br />
+      <Form.Group>
+        <Stack direction='horizontal' gap={2}>
+          <Form.Control onChange={({ target }) => { setSearch(target.value) }} value={search} className='me-auto' placeholder='Search...' />
+        </Stack>
+      </Form.Group>
+      <br />
+      {!users[0]
+        ? <div />
+        : users.filter(user => {
+          const fullname = user.name + ' ' + user.surname
+          return fullname.toLowerCase().includes(search.toLowerCase())
+        }).map(user => (
+          <Card key={user.id}>
+            <Card.Body>
+              <Stack direction='horizontal' gap={2}>
+                <img
+                  style={{
+                    width: '150px',
+                    height: '150px',
+                    resizeMode: 'contain'
+                  }}
+                  src={user.profileImg}
+                  alt={user.name}
+                />
+                <div>
+                  <Card.Title>{user.name} {user.surname}
+                    {' '}<Badge bg='info'>{user.trustRate} % trust</Badge>
+                  </Card.Title>
+                  <Card.Text>Email: {user.email}</Card.Text>
+                </div>
+              </Stack>
+            </Card.Body>
+          </Card>))}
     </Container>
   )
 }
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Navbar, Container, Nav, Form, Button } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 import { userLogout } from '../reducers/userReducer'
 import { useSelector, useDispatch } from 'react-redux'
 import logo from '../public/white-logo-grande.png'
@@ -10,7 +10,7 @@ export default function Header () {
   const dispatch = useDispatch()
 
   return (
-    <Navbar sticky='top' bg='primary' variant='dark'>
+    <Navbar collapseOnSelect expand='lg' sticky='top' bg='primary' variant='dark'>
       <Container>
         <Navbar.Brand href='/'>
           <img
@@ -22,28 +22,26 @@ export default function Header () {
           />{' '}
           eTrust
         </Navbar.Brand>
-        <Navbar.Toggle />
-        <Nav>
-          <Nav.Link href='#' as='span'>
-            <Link to='/deals' style={{ color: '#FFFFFF' }}>
-              Deals
-            </Link>
-          </Nav.Link>
-          <Nav.Link href='#' as='span'>
-            <Link to='/users' style={{ color: '#FFFFFF' }}>
-              Users
-            </Link>
-          </Nav.Link>
-          <Nav.Link href='#' as='span'>
-            <Link to='/profile' style={{ color: '#FFFFFF' }}>
-              My Profile
-            </Link>
-          </Nav.Link>
-        </Nav>
-        <Form.Control className='me-auto' placeholder='Search...' style={{ width: '400px', marginLeft: '70px' }} />
-        {' '}{' '}
-        <Button variant='light' style={{ marginLeft: '2px', color: '#0B5ED7' }}>Search</Button>
-        <Navbar.Collapse className='justify-content-end'>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#' as='span'>
+              <Link to='/deals' style={{ color: '#FFFFFF' }}>
+                Deals
+              </Link>
+            </Nav.Link>
+            <Nav.Link href='#' as='span'>
+              <Link to='/users' style={{ color: '#FFFFFF' }}>
+                Users
+              </Link>
+            </Nav.Link>
+            <Nav.Link href='#' as='span'>
+              <Link to='/profile' style={{ color: '#FFFFFF' }}>
+                My Profile
+              </Link>
+            </Nav.Link>
+          </Nav>
+
           {
               user.email
                 ? (
